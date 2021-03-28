9af7ad6129fdda4af18efb0e28755b832a3dd87b
AnastaciusWright <alexandronadal@gmail.com>
2021-03-28 20:58:54 -0300

    adapting var envs to aws requirements

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports={
     'secret': process.env.JWTSECRET,
     'database': 'mongodb://localhost/node-auth',
-    'remoteDB' : 'mongodb+srv://backendPortfolio:'+process.env.MONGO_KEY+'@nadal-cluster.9oeuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    'remoteDB' : 'mongodb+srv://backendPortfolio:'+process.env.MONGOKEY+'@nadal-cluster.9oeuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
